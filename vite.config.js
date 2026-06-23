224e60a78e529065ff2723fb6646d0b0f6399220
raneddie321 <raneddie321@gmail.com>
2026-06-23 16:35:59 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/gentouch/' : '/',
+ base: '/',
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
